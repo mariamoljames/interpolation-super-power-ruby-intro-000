8d9f69b4ca86f4f266b3507d05f23131b8919035
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-03 03:25:02 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,2 @@
-# Write your #display_rainbow method here
+def display_rainbow(colors)
+  puts "R:#{colors[0]},R:#{colors[0]},R:#{colors[0]},R:#{colors[0]},R:#{colors[0]},R:#{colors[0]},R:#{colors[0]}"
